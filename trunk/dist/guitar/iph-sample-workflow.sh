96a4e9d8a8977e75360fea33e881118a1139647a
csnowleopard <csnowleopard@gmail.com>
2013-06-03 10:33:34 -0400

    cleaned up port-freeing

--- trunk/dist/guitar/iph-sample-workflow.sh
+++ trunk/dist/guitar/iph-sample-workflow.sh
@@ -192,7 +192,7 @@ echo "Enter the number of test case(s): "
 i=0 
 for testcase in `find $testcases_dir -name "*.tst"| head -n$testcase_num`  
     do
-    	cmd2="lsof -n -i4TCP:${port} | grep LISTEN | awk '{print $2}' | xargs kill"
+    cmd2="lsof -P | grep ':${port}' | awk '{print $2}' | xargs kill"
     #kill everything running on the needed port
     eval $cmd2
     # getting test name 
